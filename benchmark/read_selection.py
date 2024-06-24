f596799b9e553a537e13596b6148edf59263d9f4
John Readey <jreadey@hdfgroup.org>
2024-06-24 11:16:01 +0000

    Add s3fs option to benchmark

--- benchmark/read_selection.py
+++ benchmark/read_selection.py
@@ -1,8 +1,10 @@
 import h5pyd
 import h5py
+import s3fs
 import random
 import sys
 import time
+
 
 def get_indices(extent, count):
     indices = set()
@@ -17,22 +19,34 @@ def get_indices(extent, count):
 # main
 #
 
-if len(sys.argv) == 1 or sys.argv[1] in ("-h", "--help"):
-    usage = f"Usage:  {sys.argv[0]} <filepath>\n"
-    usage += f"Example: python {sys.argv[0]} hdf5://nrel/nsrdb/conus/nsrdb_conus_2022.h5"
+usage = f"Usage:  {sys.argv[0]} [s3fs | hsds]"
+
+if len(sys.argv) > 1 and sys.argv[1] in ("-h", "--help"):
     sys.exit(usage)
 
-filepath = sys.argv[1]
-
 h5path =  "wind_speed"  # change if this dataset is not in the file
-bucket = "s3://nrel-pds-hsds"  # bucket for NREL HSDS data, ignored for posix file paths
 
-if filepath.startswith("hdf5://"):
-    f = h5pyd.File(filepath, bucket=bucket)
+if len(sys.argv) > 1:
+    if sys.argv[1] not in ("s3fs", "hsds"):
+        sys.exit(usage)
+    driver = sys.argv[1]
 else:
-    f = h5py.File(filepath)
+    driver = "hsds"  # default to hsds
 
-print(list(f))
+if driver == "s3fs":
+    # s3fs enables h5py to "see" S3 files as read-only posix files
+    s3_uri = "s3://nrel-pds-nsrdb/conus/nsrdb_conus_pv_2022.h5"
+    s3 = s3fs.S3FileSystem(anon=True)
+    print(f"opening HDF5 file at: {s3_uri} with s3fs")
+    f = h5py.File(s3.open("s3://nrel-pds-nsrdb/conus/nsrdb_conus_pv_2022.h5", "rb"), "r")
+else:
+    hsds_domain = "hdf5://nrel/nsrdb/conus/nsrdb_conus_2022.h5"
+    hsds_bucket = "s3://nrel-pds-hsds"  # bucket for NREL HSDS data
+    print(f"opening HSDS domain: {hsds_domain} on bucket: {hsds_bucket}")
+    f = h5pyd.File(hsds_domain, bucket=hsds_bucket)
+
+if h5path not in f:
+    sys.exit(f"{h5path} dataset not found")
 
 dset = f[h5path]
 print(f"{h5path}: {dset}")
